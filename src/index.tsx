10636fa0d50468ab2e37370346872e23b24dd26d
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-08-08 15:11:38 -0300

    Tailwindcss
    Installing and configuring tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import './styles/global.css'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
