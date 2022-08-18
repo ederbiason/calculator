f8344f79056bd80ed653da5c4c7c08d7fa649880
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-08-18 15:01:11 -0300

    Structure
    Just the template of the calculator

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonCard, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import './Tab1.css'
 
 const Tab1: React.FC = () => {
   return (
@@ -9,13 +9,143 @@ const Tab1: React.FC = () => {
           <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonCard id="textArea">
+          visor da calculadora
+        </IonCard>
+
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonButton expand="full" shape="round" color="secondary">
+                  C
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="secondary">
+                  (
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="secondary">
+                  )
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="secondary">
+                  +
+              </IonButton>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  7
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  8
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  9
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="secondary">
+                  -
+              </IonButton>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  4
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  5
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  6
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="secondary">
+                  x
+              </IonButton>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  1
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  2
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  3
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="secondary">
+                  ÷
+              </IonButton>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonButton expand="full" shape="round" color="secondary">
+                  Del
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  0
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="dark">
+                  .
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton expand="full" shape="round" color="secondary">
+                  =
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
